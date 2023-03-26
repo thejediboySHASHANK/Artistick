36397e8c08f3b842be1db92078da74ff6188a3b2
thejediboySHASHANK <21052358@kiit.ac.in>
2023-03-26 18:53:16 +0530

    aesthetic

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,7 +1,7 @@
 export default function Image({src, ...rest}) {
     src = src && src.includes('https://')
         ? src
-        : 'http://localhost:4000/API/uploads/'+src
+        : 'http://localhost:4000/api/uploads/'+src
     return (
         <img {...rest} src={src} alt={''} />
     )
--- client/src/pages/IndexPage.jsx
+++ client/src/pages/IndexPage.jsx
@@ -24,7 +24,7 @@ export default function IndexPage() {
         <div className="mb-10 md:-mb-10">
             <HeroAnimation />
             <div className="spacing"><br/></div>
-            <h2 className="text-3xl flex justify-center gap-5 border border-gray-500 bg-gray-100 rounded-2xl py-3">
+            <h2 className="text-3xl flex justify-center gap-5 italic rounded-2xl py-3">
                 <div className="py-1">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
                         <path fillRule="evenodd" d="M14.615 1.595a.75.75 0 01.359.852L12.982 9.75h7.268a.75.75 0 01.548 1.262l-10.5 11.25a.75.75 0 01-1.272-.71l1.992-7.302H3.75a.75.75 0 01-.548-1.262l10.5-11.25a.75.75 0 01.913-.143z" clipRule="evenodd" />
